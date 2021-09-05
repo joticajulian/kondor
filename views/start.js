f31314866ddf35a6b66e60741b654b31ba75ac78
Julian Gonzalez <joticajulian@gmail.com>
2021-09-05 02:23:34 +0200

    using password

--- views/start.js
+++ views/start.js
@@ -12,25 +12,55 @@ function toHexString(buffer) {
     .join("");
 }
 
-const messageResponses = [];
-chrome.runtime.onMessage.addListener((msg, sender, sendResp) => {
-  messageResponses.push(msg);
-  sendResp(true);
-});
+const db = {
+  get: (keys) =>
+    new Promise((resolve, reject) => {
+      chrome.storage.local.get(keys, function (result) {
+        if (Object.keys(result).length == 0) resolve(null);
+        else resolve(result);
+      });
+    }),
+  set: (data) =>
+    new Promise((resolve, reject) => {
+      chrome.storage.local.set(data, function () {
+        resolve();
+      });
+    }),
+};
 
-async function sendMessage(method, params) {
-  const id = Math.round(Math.random() * 1000);
-  chrome.runtime.sendMessage({ id, method, params });
-  while (true) {
-    await new Promise((resolve) => setTimeout(resolve, 20));
-    const i = messageResponses.findIndex((m) => m.id === id);
-    if (i >= 0) {
-      const responses = messageResponses.splice(i, 1);
-      const { error, result } = responses[0];
-      if (error) throw new Error(error.message);
-      return result;
-    }
+async function getAccounts() {
+  const result = await db.get(["accounts"]);
+  if (!result) return null;
+  return result.accounts;
+}
+
+async function getRpcNode() {
+  let result = await db.get(["rpcNode"]);
+  if (!result) {
+    // store default value
+    await db.set({rpcNode: "http://159.203.119.0:8080"});
+    result = await db.get(["rpcNode"]);
   }
+  return result.rpcNode;
+}
+
+async function storeAccount(encrypted) {
+  return await db.set({ accounts: encrypted });
+}
+
+async function getOptsEncryption() {
+  let result = await db.get(["salt", "iv"]);
+  if (!result) {
+    const salt = window.crypto.getRandomValues(new Uint8Array(16));
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    await db.set({ salt, iv });
+    result = await db.get(["salt", "iv"]);
+    if (!result)
+      throw new Error("Local storage error: cannot save salt and iv");
+  }
+  const salt = new Uint8Array(Object.keys(result.salt).map((k) => result.salt[k])).buffer;
+  const iv = new Uint8Array(Object.keys(result.iv).map((k) => result.iv[k])).buffer;
+  return { salt, iv };
 }
 
 function getKeyMaterial(password) {
@@ -44,7 +74,8 @@ function getKeyMaterial(password) {
   );
 }
 
-function getKey(keyMaterial, salt) {
+async function getKey(password, salt) {
+  const keyMaterial = await getKeyMaterial(password);
   return window.crypto.subtle.deriveKey(
     {
       name: "PBKDF2",
@@ -59,23 +90,14 @@ function getKey(keyMaterial, salt) {
   );
 }
 
-async function encrypt(data, password) {
-  let salt = window.crypto.getRandomValues(new Uint8Array(16));
-  let iv = window.crypto.getRandomValues(new Uint8Array(12));
-  const opts = await sendMessage("getOptsEncryption", { salt, iv });
-  salt = new Uint8Array(Object.keys(opts.salt).map((k) => opts.salt[k])).buffer;
-  iv = new Uint8Array(Object.keys(opts.iv).map((k) => opts.iv[k])).buffer;
-
-  const keyMaterial = await getKeyMaterial(password);
-  const key = await getKey(keyMaterial, salt);
+async function encrypt(data, password) {  
+  const { salt, iv } = await getOptsEncryption();
+  const key = await getKey(password, salt);
   const message = JSON.stringify(data);
   const encoded = new TextEncoder().encode(message);
 
   const bufferEncrypted = await window.crypto.subtle.encrypt(
-    {
-      name: "AES-GCM",
-      iv: iv,
-    },
+    { name: "AES-GCM", iv },
     key,
     encoded
   );
@@ -83,19 +105,12 @@ async function encrypt(data, password) {
 }
 
 async function decrypt(encrypted, password) {
-  const opts = await sendMessage("getOptsEncryption", "decrypt");
-  const salt = new Uint8Array(Object.keys(opts.salt).map((k) => opts.salt[k]))
-    .buffer;
-  const iv = new Uint8Array(Object.keys(opts.iv).map((k) => opts.iv[k])).buffer;
-  const keyMaterial = await getKeyMaterial(password);
-  const key = await getKey(keyMaterial, salt);
+  const { salt, iv } = await getOptsEncryption();
+  const key = await getKey(password, salt);
   const bufferEncrypted = toUint8Array(encrypted);
 
   const decrypted = await window.crypto.subtle.decrypt(
-    {
-      name: "AES-GCM",
-      iv,
-    },
+    { name: "AES-GCM", iv },
     key,
     bufferEncrypted
   );
@@ -103,20 +118,17 @@ async function decrypt(encrypted, password) {
   return JSON.parse(message);
 }
 
-let seed;
-let salt, iv;
-let accounts;
 let wallet;
 
 const viewUnlock = document.getElementById("view-unlock");
 const viewImport = document.getElementById("view-import");
 const viewTransfer = document.getElementById("view-transfer");
 const inputPrivateKey = document.getElementById("private-key");
-const inputSetPassword = document.getElementById("set-password");
+//const inputSetPassword = document.getElementById("set-password");
 const inputPassword = document.getElementById("password");
 const buttonImport = document.getElementById("route-pkey");
 const buttonGotoImport = document.getElementById("goto-import");
-const buttonUnlock = document.getElementById("unlock");
+//const buttonUnlock = document.getElementById("unlock");
 const textBalanceValue = document.getElementById("balance-value");
 const textAddress = document.getElementById("address");
 const inputTransferTo = document.getElementById("send-address");
@@ -124,25 +136,7 @@ const inputTransferAmount = document.getElementById("send-amount");
 //const buttonTransfer = document.getElementById("transfer");
 const textAlert = document.getElementById("text-alert");
 
-async function init() {
-  const accounts = await sendMessage("getAccounts");
-  viewTransfer.classList.add("hidden");
-  if (accounts) {
-    viewUnlock.classList.remove("hidden");
-    viewImport.classList.add("hidden");
-  } else {
-    viewUnlock.classList.add("hidden");
-    viewImport.classList.remove("hidden");
-  }
-}
-init();
-
-buttonGotoImport.addEventListener("click", async () => {
-  viewUnlock.classList.add("hidden");
-  viewImport.classList.remove("hidden");
-});
-
-buttonUnlock.addEventListener("click", async () => {
+/*buttonUnlock.addEventListener("click", async () => {
   try {
     viewTransfer.classList.remove("hidden");
     viewUnlock.classList.add("hidden");
@@ -155,13 +149,14 @@ buttonUnlock.addEventListener("click", async () => {
     textAlert.innerText = error.message;
     console.log(error);
   }
-});
+});*/
 
 async function loadViewAccount(privateKey) {
   if (!privateKey) throw new Error("private key not defined");
+  const rpcNode = await getRpcNode();
   wallet = new Wallet({
     signer: Signer.fromWif(privateKey),
-    provider: new Provider("http://159.203.119.0:8080"),
+    provider: new Provider(rpcNode),
     contract: new Contract({
       id: "Mkw96mR+Hh71IWwJoT/2lJXBDl5Q=",
       entries: {
@@ -201,7 +196,7 @@ async function loadViewAccount(privateKey) {
   textBalanceValue.innerText = numberBalance.toLocaleString('en');
 }
 
-buttonImport.addEventListener("click", async () => {
+/*buttonImport.addEventListener("click", async () => {
   viewTransfer.classList.remove("hidden");
   viewImport.classList.add("hidden");
   try {
@@ -218,7 +213,7 @@ buttonImport.addEventListener("click", async () => {
     textAlert.innerText = error.message;
     console.error(error);
   }
-});
+});*/
 
 function getSatoshis(value, decimals) {
   if (isNaN(Number(value))) throw new Error(`Invalid value ${value}`);
