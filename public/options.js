03dc5b7885622da330e6eb0c0681a50b55b1a108
Julian Gonzalez <joticajulian@gmail.com>
2022-01-01 08:06:45 +0100

    lint and remove unused files

--- public/options.js
+++ public/options.js
@@ -5,9 +5,9 @@ const textAlert = document.getElementById("text-alert");
 (async () => {
   const rpcNode = await getRpcNode();
   inputRpcNode.value = rpcNode;
-})()
+})();
 
 buttonSetRpcNode.onclick = async () => {
   await setRpcNode(inputRpcNode.value);
-  textAlert.innerText = "Data saved!"
+  textAlert.innerText = "Data saved!";
 };
