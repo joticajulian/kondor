1966fd5dfd25cab94cfb7980d2fa3f6a82bc1cd0
Julian Gonzalez <joticajulian@gmail.com>
2022-01-10 17:11:02 +0100

    temp async messages

--- public/js/background.js
+++ public/js/background.js
@@ -1,5 +1,36 @@
 let tabId;
+const msgPool = [];
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  const { id, data } = request;
+  sendResponse({});
+  const i = this.msgPool.findIndex(m => m.id === id);
+  if (i >= 0) {
+    // this is a response from a previous message
+    this.msgPool[i].response = data;
+    console.log("bg: response");
+    console.log(data);
+    return;
+  }
+
+  // processing the request
+  if (sender.tab) { // reply to app
+
+  } else { // reply to extension
+    chrome.runtime.sendMessage({
+      id,
+      data: {
+        test: "response ok",
+      }
+    });
+  }
+  
+  
+  
+  
+  
+  
+  
+  
   console.log(request.greeting);
   if (request.greeting === "hi background, I'm tab") {
     console.log("response: hey tab!");
