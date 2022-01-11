d5e43c3a755df50ec6014c42ac6827d4c8bfe7eb
Julian Gonzalez <joticajulian@gmail.com>
2022-01-11 17:03:45 +0100

    improvement async messaging with popups

--- public/js/background.js
+++ public/js/background.js
@@ -1,57 +1,73 @@
-let tabId;
+let tabIdRequester;
 const msgPool = [];
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+let ii=0;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const { id, data } = request;
-  sendResponse({});
-  const i = this.msgPool.findIndex(m => m.id === id);
+  sendResponse({}); ii++; if (ii>=20) return;
+  const i = msgPool.findIndex(m => m.id === id);
   if (i >= 0) {
     // this is a response from a previous message
-    this.msgPool[i].response = data;
-    console.log("bg: response");
-    console.log(data);
+    msgPool[i].response = data;
     return;
   }
 
   // processing the request
-  if (sender.tab) { // reply to app
-
+  if (sender.tab) { // reply to tab
+    processRequestFromTab(sender.tab.id, request);
   } else { // reply to extension
-    chrome.runtime.sendMessage({
-      id,
-      data: {
-        test: "response ok",
-      }
-    });
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  console.log(request.greeting);
-  if (request.greeting === "hi background, I'm tab") {
-    console.log("response: hey tab!");
-    sendResponse({ farewell: "hey tab!" });
-    tabId = sender.tab.id;
-
-    chrome.windows.create(
-      {
-        focused: true,
-        height: 500,
-        width: 309,
-        type: "popup",
-        url: "index.html",
-        top: 0,
-        left: 0,
-      },
-      () => {}
-    );
-  }
-  if (request.greeting === "hi background, I'm popup") {
-    console.log("response: " + tabId);
-    sendResponse({ tabId });
+    // processRequestFromExtension(request);
   }
 });
+
+async function sendMessage(to, data) {
+  const id = Math.round(Math.random() * 10000);
+  msgPool.push({id});
+
+  if (to === "extension") {
+    chrome.runtime.sendMessage({ id, data });
+  } else { // 'to' is tab.id
+    chrome.tabs.sendMessage(to, { id, data });
+  }
+  
+  let i = msgPool.findIndex(m => m.id === id);
+  while ( !msgPool[i].response ) {
+    await new Promise(r => setTimeout(r, 20));
+    i = msgPool.findIndex(m => m.id === id);
+  }
+  const [ msgResp ] = msgPool.splice(i, 1);
+  if (msgResp.response.error) throw new Error(msgResp.response.error);
+  return msgResp.response;
+};
+
+async function processRequestFromTab(tabId, request) {
+  const { id, data } = request;
+  const { command } = data;
+  switch (command) {
+    case "openPopup":
+      tabIdRequester = tabId;
+      chrome.tabs.sendMessage(tabId, { id, data: {
+        test: "response ok 30",
+      }});
+
+      chrome.windows.create(
+        {
+          focused: true,
+          height: 500,
+          width: 309,
+          type: "popup",
+          url: "index.html",
+          top: 0,
+          left: 0,
+        },
+        () => {}
+      );
+
+      break;
+    case "getTab":
+      chrome.tabs.sendMessage(tabId, { id, data: { tabId: tabIdRequester }});
+      break;
+    default:
+      break;
+  }
+};
--- public/js/content.js
+++ public/js/content.js
@@ -1,14 +1,62 @@
-chrome.runtime.sendMessage(
-  { greeting: "hi background, I'm tab" },
-  function (response) {
-    console.log(response.farewell);
-  }
-);
+const msgPool = [];
+let popupLoaded = false;
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(request.greeting);
-  if (request.greeting === "hi tab, I'm popup") {
-    console.log("response: hey popup!");
-    sendResponse({ farewell: "hey popup!" });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const { id, data } = request;
+  sendResponse({});
+  const i = msgPool.findIndex(m => m.id === id);
+  if (i >= 0) {
+    // this is a response from a previous message
+    msgPool[i].response = data;
+    return;
   }
+  processRequestFromExtension(request);
 });
+
+async function sendMessage(to, data) {
+  const id = Math.round(Math.random() * 10000);
+  msgPool.push({id});
+
+  //if (to === "extension") {
+    chrome.runtime.sendMessage({ id, data });
+  //} else { // 'to' is tab.id
+  //  chrome.tabs.sendMessage(to, { id, data });
+  //}
+  
+  let i = msgPool.findIndex(m => m.id === id);
+  while ( !msgPool[i].response ) {
+    await new Promise(r => setTimeout(r, 20));
+    i = msgPool.findIndex(m => m.id === id);
+  }
+  const [ msgResp ] = msgPool.splice(i, 1);
+  if (msgResp.response.error) throw new Error(msgResp.response.error);
+  return msgResp.response;
+};
+
+async function processRequestFromExtension(request) {
+  const { id, data } = request;
+  const { command } = data;
+  switch (command) {
+    case "popupLoaded":
+      chrome.runtime.sendMessage({ id, data: {
+        test: "response ok 200",
+      }});
+      popupLoaded = true;
+      break;
+    default:
+      break;
+  }
+};
+
+(async () => {
+  console.log("calling bg to openPopup");
+  const response1 = await sendMessage("background", { command: "openPopup"});
+  console.log("response from bg")
+  console.log(response1);
+  while(!popupLoaded) await new Promise(r => setTimeout(r, 20));
+  console.log("popup uploaded");
+  console.log("sending a message to the popup")
+  const response2 = await sendMessage("extension", { command: "newWallet" })
+  console.log("response from extension")
+  console.log(response2);
+})()
