1b55dbfec0cd387588a7721712b6103a8efaee94
Julian Gonzalez <joticajulian@gmail.com>
2022-02-08 01:43:44 +0100

    unlock component, sign transaction - #11

---
+++ src/index/components/Unlock.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="unlock">unlock</button>
+  </div>
+</template>
+
+<script>
+import Storage from "@/shared/mixins/Storage";
+
+export default {
+  name: "Unlock",
+  data() {
+    return {
+      password: "",
+    };
+  },
+  mixins: [Storage],
+
+  methods: {
+    async unlock() {
+      console.log("click unlock");
+      try {
+        const enc = await this.getAccounts();
+        const { privateKey } = await this.decrypt(enc, this.password);
+        this.$store.state.privateKey = privateKey;
+        this.$emit("onUnlock");
+        console.log("unlock ok");
+      } catch (error) {
+        this.$emit("onError", error);
+        console.log("unlock error");
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
