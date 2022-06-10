f0f9fad66f063d49d5963db5a0bad79853378ccd
Julian Gonzalez <joticajulian@gmail.com>
2022-06-10 18:19:25 +0200

    refactor store files

--- src/options/main.js
+++ src/options/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store from "@/shared/store";
 import "@/shared/assets/css/kondor.css";
 import "./assets/options.css";
 
