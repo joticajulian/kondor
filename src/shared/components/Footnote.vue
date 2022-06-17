d66950eefb3cad622fa5609fdc180cc785484c3a
Julian Gonzalez <joticajulian@gmail.com>
2022-06-17 19:28:46 +0200

    warning message for old versions of koilib and kondor

---
+++ src/shared/components/Footnote.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  padding: 0.8em;
+  font-size: 0.7em;
+  color: black;
+  margin: 5px 0px;
+  background-color: orange;
+}
+</style>
