a7c596821dddcf1f76b01dae36b181e880268ed0
Julian Gonzalez <joticajulian@gmail.com>
2022-11-08 18:09:03 +0100

    setup dev mode icons

--- manifest.js
+++ manifest.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
+const dotenv = require("dotenv");
 const packageJson = require("./package.json");
+dotenv.config();
+
+const testMode = process.argv[2] === "development";
+
+const icons = {
+  16: testMode ? "/images/kondor-dev-16.png" : "/images/kondor-16.png",
+  32: testMode ? "/images/kondor-dev-32.png" : "/images/kondor-32.png",
+  48: testMode ? "/images/kondor-dev-48.png" : "/images/kondor-48.png",
+  128: testMode ? "/images/kondor-dev-128.png" : "/images/kondor-128.png",
+};
 
 const manifest = {
   name: "Kondor",
   description: "Koinos Wallet in your browser",
-  version: packageJson.version,
+  version: testMode ? "0.0.0" : packageJson.version,
   manifest_version: 3,
   permissions: ["storage"],
   background: {
@@ -18,22 +29,12 @@ const manifest = {
   ],
   action: {
     default_popup: "index.html",
-    default_icon: {
-      16: "/images/kondor-16.png",
-      32: "/images/kondor-32.png",
-      48: "/images/kondor-48.png",
-      128: "/images/kondor-128.png",
-    },
+    default_icon: icons,
   },
   sandbox: {
     pages: ["sandbox.html"],
   },
-  icons: {
-    16: "/images/kondor-16.png",
-    32: "/images/kondor-32.png",
-    48: "/images/kondor-48.png",
-    128: "/images/kondor-128.png",
-  },
+  icons,
   options_page: "options.html",
 };
 
