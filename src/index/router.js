aa4241b7a680ccb683e4d03982de3293290223e8
Julian Gonzalez <joticajulian@gmail.com>
2022-11-10 00:25:01 +0100

    confirm seed

--- src/index/router.js
+++ src/index/router.js
@@ -5,6 +5,7 @@ import NewWallet from "./views/2-NewWallet.vue";
 import ImportSeedPhrase from "./views/3a-ImportSeedPhrase";
 import ImportPrivateKey from "./views/3b-ImportPrivateKey";
 import GenerateSeed from "./views/3c-GenerateSeed";
+import ConfirmSeed from "./views/3d-ConfirmSeed";
 import Dashboard from "./views/4-Dashboard.vue";
 import Signers from "./views/5-Signers.vue";
 import CreateAccount from "./views/setting-CreateAccount";
@@ -37,6 +38,11 @@ export default new Router({
       path: "/generateSeed",
       name: "GenerateSeed",
       component: GenerateSeed,
+    },
+    {
+      path: "/confirmSeed",
+      name: "ConfirmSeed",
+      component: ConfirmSeed,
     },
     {
       path: "/dashboard",
