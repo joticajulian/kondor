30c8f0f3a241b1ceb899ee83055bc3a0e0902fe3
Julian Gonzalez <joticajulian@gmail.com>
2023-03-04 21:36:28 +0100

    fix lint

--- src/index/components/Nav.vue
+++ src/index/components/Nav.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <div v-if="$store.state.showBackButton" class="header">
-      <div class="back-button" @click="back">&#8592;</div>
-      <div class="lock-button">Lock</div>
+    <div
+      v-if="$store.state.showBackButton"
+      class="header"
+    >
+      <div
+        class="back-button"
+        @click="back"
+      >
+        &#8592;
+      </div>
+      <div class="lock-button">
+        Lock
+      </div>
       <div v-if="$store.state.network">
-        <span class="connection-indicator">&#9724;</span
-        >{{ $store.state.network }}
+        <span class="connection-indicator">&#9724;</span>{{ $store.state.network }}
       </div>
     </div>
     <AccountMenu v-if="$store.state.showAccountMenu" />
@@ -13,18 +22,18 @@
 </template>
 
 <script>
-import router from "@/index/router"
-import AccountMenu from "@/index/components/AccountMenu.vue"
+import router from "@/index/router";
+import AccountMenu from "@/index/components/AccountMenu.vue";
 
 export default {
   components: { AccountMenu },
 
   methods: {
     back() {
-      router.back()
+      router.back();
     },
   },
-}
+};
 </script>
 <style scoped>
 .back-button {
